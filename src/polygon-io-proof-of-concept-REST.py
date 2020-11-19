98e2fbfcf2e94f6bde80b95f76dee3618219765d
nscherer30 <nscherer30@gmail.com>
2020-11-18 19:04:40 -0800

    polygon api REST call returns apple

---
+++ src/polygon-io-proof-of-concept-REST.py
@@ -0,0 +1,15 @@
+from polygon import RESTClient
+
+
+def main():
+    key = "w8NGBOgXjp393pMbIBhHO6sOxN_N1x8E"
+
+    # RESTClient can be used as a context manager to facilitate closing the underlying http session
+    # https://requests.readthedocs.io/en/master/user/advanced/#session-objects
+    with RESTClient(key) as client:
+        resp = client.stocks_equities_daily_open_close("AAPL", "2018-03-02")
+        print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
+
+
+if __name__ == '__main__':
+    main()
