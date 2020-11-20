f19303138e2ba89565fb70bcb598fd24cb0fd82a
nscherer30 <nscherer30@gmail.com>
2020-11-20 15:12:07 -0800

    removed irrelevant files

--- src/simple_scraper.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'https://stackoverflow.com/questions/529034/python-pass-or-sleep-for-long-running-processes'
-response = requests.get(url)
-html = response.content
-print(html)
