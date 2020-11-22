e63a427b4d6a37691674d58cc9be8b124f67b2f9
nscherer30 <nscherer30@gmail.com>
2020-11-22 01:59:13 -0800

    successful key match from jim cramer twitter with email notification

--- src/twitter-jim-cramer-test-using-web.py
+++ src/twitter-jim-cramer-test-using-web.py
@@ -1,4 +1,5 @@
 import time
+import config
 from webbot import Browser
 
 web = Browser()
@@ -12,8 +13,40 @@ testKeyMatch = 'concepts'
 
 for element in resultElements:
     print(element.text)
-    if(element.text.contains(testKeyMatch)):
+    if testKeyMatch in element.text:
         #match found
+        # Import requests (to download the page)
+        import requests
+
+        # Import smtplib (to allow us to email)
+        import smtplib
+
+        # while this is true (it is true by default),
+        while True:
+            msg = f'Subject: Found {testKeyMatch} from Jim Cramer Tweet.  Full Tweet below'
+            # set the 'from' address,
+            fromaddr = config.fromEmail
+
+
+            # setup the email server,
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            # add my account login name and password,
+            server.login(fromaddr, config.gmailLoginAppPass)
+
+            # Print the email's contents
+            print('From: ' + fromaddr)
+            print('To: ' + str(config.emailList))
+            print('Message: ' + msg)
+
+            # send the emai
+            server.sendmail(fromaddr, config.emailList, msg)
+            # disconnect from the server
+            server.quit()
+
+            break
+
+
 
 
     print(element.tag_name)
