c340b1a52949981e32d3dd0eca38ed168b5e3a97
nscherer30 <nscherer30@gmail.com>
2020-12-01 21:13:30 -0800

    added module

--- finance/post-to-discord-POC.py
+++ finance/post-to-discord-POC.py
@@ -1,6 +1,7 @@
 
 import requests
+import secrets
 from discord import Webhook, RequestsWebhookAdapter
 
-webhook = Webhook.from_url("https://discord.com/api/webhooks/783558803601358849/LdnpgLiJ9y20_haK-81w_P8JbjzX1f664VO2dBcZgBSNoeOw52GXGezHWq33illwrMvm", adapter=RequestsWebhookAdapter())
+webhook = Webhook.from_url(secrets.discord_hook, adapter=RequestsWebhookAdapter())
 webhook.send("Hello World")
