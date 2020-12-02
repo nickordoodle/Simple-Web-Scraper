1b712d422e9a06984eb8142971cbb0221c9bcb92
nscherer30 <nscherer30@gmail.com>
2020-12-01 20:52:56 -0800

    minor changes

--- finance/retrieve-tweets-by-query-POC.py
+++ finance/retrieve-tweets-by-query-POC.py
@@ -1,14 +1,10 @@
-# gikaEFjn2Hf9beqp1zuuYxBQm
 import os
+import secrets
 import tweepy as tw
 import pandas as pd
 
-consumer_key= 'gikaEFjn2Hf9beqp1zuuYxBQm'
-consumer_secret= 'diy4YXK8SYBTlsSSPtHQcyElknt6U1JnX4brGshrzFUKWcypEY'
-access_token= 'yourkeyhere'
-access_token_secret= 'yourkeyhere'
 
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth = tw.OAuthHandler(secrets.twitter_consumer_key, secrets.twitter_consumer_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
 # Define the search term and the date_since date as variables
